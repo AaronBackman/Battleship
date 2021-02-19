57584e253e10bdfa39da3007d256f124d6c6c68a
AaronBackman <aaron.a.backman@gmail.com>
2021-02-19 17:52:34 +0200

    stage 1 is now complete (selecting and confirming options)

--- src/Game.js
+++ src/Game.js
@@ -8,12 +8,19 @@ class Game extends React.Component {
       gameTurn: 0,
       beginningTurn: 0,
       reset: false,
-      gameState: {
+
+      boards: {
         player1Board: [],
         player2Board: [],
+      },
+
+      ships: {
         player1Ships: [],
         player2Ships: [],
       },
+
+      shipCount: [0, 0, 0, 0, 0],
+
       boardSize: 5,
       player1Name: '',
       player2Name: '',
@@ -21,13 +28,14 @@ class Game extends React.Component {
   }
 
   render() {
-    // first stage in a completely new game
+    // first stage in a new game
     if (this.state.gameTurn === 0 && this.state.beginningTurn === 0) {
       return <OptionsWindow
         setState={p => this.setState(p)}
         player1Name={this.state.player1Name}
         player2Name={this.state.player2Name}
         boardSize={this.state.boardSize}
+        shipCount={this.state.shipCount}
       />
     }
 
@@ -37,8 +45,15 @@ class Game extends React.Component {
   }
 }
 
+// makes an initial board as 2d array consisting of default (empty) squares
 function initGameBoard(boardSize) {
-
+  return Array(boardSize).fill(0).map(x => Array(boardSize).fill(0).map(x => {
+    return {
+      fog: false,
+      shot: false,
+      noShip: true,
+    }
+  }));
 }
 
 export default Game;
--- src/OptionsWindow.js
+++ src/OptionsWindow.js
@@ -60,9 +60,122 @@ class OptionsWindow extends React.Component {
           </div>
         </div>
 
+        <div className="ship-selection-container">
+
+          <div className="ship-selection">
+            <div>Aircraft Carriers:</div>
+            <input className="carrier-input"
+              value={this.props.shipCount[0]}
+              onChange={(e) => {
+                const newValue = e.target.value;
+
+                // cant have less than 0 ships
+                if (newValue < 0) return;
+
+                const copyArr = JSON.parse(JSON.stringify(this.props.shipCount));
+                copyArr[0] = newValue;
+
+                setState({shipCount: copyArr});
+              }}
+            />
+          </div>
+
+          <div className="ship-selection">
+            <div>Battleships:</div>
+            <input className="battleship-input"
+              value={this.props.shipCount[1]}
+              onChange={(e) => {
+                const newValue = e.target.value;
+
+                // cant have less than 0 ships
+                if (newValue < 0) return;
+
+                const copyArr = JSON.parse(JSON.stringify(this.props.shipCount));
+                copyArr[1] = newValue;
+
+                setState({shipCount: copyArr});
+              }}
+            />
+          </div>
+
+          <div className="ship-selection">
+            <div>Cruisers:</div>
+            <input className="cruiser-input"
+              value={this.props.shipCount[2]}
+              onChange={(e) => {
+                const newValue = e.target.value;
+
+                // cant have less than 0 ships
+                if (newValue < 0) return;
+
+                const copyArr = JSON.parse(JSON.stringify(this.props.shipCount));
+                copyArr[2] = newValue;
+
+                setState({shipCount: copyArr});
+              }}
+            />
+          </div>
+
+          <div className="ship-selection">
+            <div>Submarines:</div>
+            <input className="submarine-input"
+              value={this.props.shipCount[3]}
+              onChange={(e) => {
+                const newValue = e.target.value;
+
+                // cant have less than 0 ships
+                if (newValue < 0) return;
+
+                const copyArr = JSON.parse(JSON.stringify(this.props.shipCount));
+                copyArr[3] = newValue;
+
+                setState({shipCount: copyArr});
+              }}
+            />
+          </div>
+
+          <div className="ship-selection">
+            <div>Destroyers:</div>
+            <input className="destroyer-input"
+              value={this.props.shipCount[4]}
+              onChange={(e) => {
+                const newValue = e.target.value;
+
+                // cant have less than 0 ships
+                if (newValue < 0) return;
+
+                const copyArr = JSON.parse(JSON.stringify(this.props.shipCount));
+                copyArr[4] = newValue;
+
+                setState({shipCount: copyArr});
+              }}
+            />
+          </div>
+
+        </div>
+
+        <div className="confirm-container">
+          <div onClick={() => {
+            const shipAreaSum = calculateShipSum(this.props.shipCount);
+
+            if (this.props.boardSize * this.props.boardSize < 2 * shipAreaSum) return;
+
+            this.props.setState({beginningTurn: 1});
+          }}>
+            Confirm
+          </div>
+
+          <div>{calculateShipSum(this.props.shipCount)}</div>
+        </div>
+
       </div>
     );
   }
+}
+
+// calculates the total area taken by the selected ships
+function calculateShipSum(shipArr) {
+  return shipArr[0] * 5 + shipArr[1] * 4 + shipArr[2] * 3 + shipArr[3] * 3 + shipArr[4] * 2;
 }
 
 export default OptionsWindow;
